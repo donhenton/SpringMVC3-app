99f0a21d0b554672aa68ee0c21cd1e0a14e8ca03
d <dhenton@localhost.com>
2015-08-07 09:05:48 -0500

    adding testing pages

---
+++ src/main/webapp/testing_demos/public_html/json/testimage_metadata.js
@@ -0,0 +1,10 @@
+var testData = 
+{
+  "date" : "08/06/2015 09:11:55",
+  "testData" : [ {
+    "testDescription" : "ScreenShotComparisonTest",
+    "goldFile" : "testing_demos/gold_files/appsample/springmvc-mainpage.png",
+    "newImageFile" : "testing_demos/public_html/images/newImages/springmvc-mainpage.png",
+    "compareFile" : "testing_demos/public_html/images/comp_images/springmvc-mainpage_comparison.png"
+  } ]
+}
